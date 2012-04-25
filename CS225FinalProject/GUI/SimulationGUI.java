cefcf3f1bab7589408009904d54a0f3cec0699dc
Eric Santana <>
2012-04-25 10:28:47 -0400

    i provided a little example for static logic

--- CS225FinalProject/GUI/SimulationGUI.java
+++ CS225FinalProject/GUI/SimulationGUI.java
@@ -3,11 +3,25 @@ package CS225FinalProject.GUI;
 //basically you can use data 
 //object to access SimulationDataStructure
 // for example data.validateLogIn() right away without initialize;
-
+import CS225FinalProject.DataStructure.StudentRecord;
+import CS225FinalProject.SimulationManager;
 import static CS225FinalProject.SimulationManager.*;
+import javax.swing.JFrame;
 /**
  */
-public class SimulationGUI {
+public class SimulationGUI extends JFrame{
+    public StudentRecord currentStudentRecord;
+    public SimulationGUI() {
+    }
+    
+    @Override
+    public void setVisible(boolean b){
+        super.setVisible(b);
+        if(b)
+            currentStudentRecord = SimulationManager.currentStudentRecord;//sample of instant access
+        
+    }
+    
     
 }
 
