65fc76b2f7328c9602ae8a4d6815c9ef47ec04a8
Eric Santana <>
2012-04-24 21:33:34 -0400

    this package is for the evaluator. i provided some empty methods

---
+++ CS225FinalProject/Validators/Evaluator.java
@@ -0,0 +1,15 @@
+package CS225FinalProject.Validators;
+//basically you can use data 
+//object to access SimulationDataStructure
+// for example data.validateLogIn() right away without initialize;
+
+import static CS225FinalProject.SimulationManager.*;
+
+
+import javax.swing.JTable;
+
+/**
+ */
+public class Evaluator {
+}
+
