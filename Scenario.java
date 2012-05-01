08aaa8b7aee9a7f775492f67d5dc285d4cb7cd50
kevinmee <kevinmee@192.168.226.28>
2012-05-01 18:58:40 -0400

    Added methods for description

--- Scenario.java
+++ Scenario.java
@@ -6,11 +6,20 @@
 public class Scenario {
     private String description, patientName;
     private PatientRecord patientRecord;
+    private SimulationController controller;
     
     public Scenario(){
-        description = "";
+        description = controller.getDescription();
         patientRecord = new PatientRecord();
         patientName = patientRecord.getName();
     }
+    
+    public String getDescription(){
+        return description;
+    }
+    public void setDescription(String descript){
+        description = descript;
+    }
+    
     
 }
