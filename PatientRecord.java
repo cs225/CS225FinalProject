a626426c34b078ef990383337d7107150215fffc
kevinmee <kevinmee@192.168.226.28>
2012-05-01 15:32:09 -0400

    added some variables

--- PatientRecord.java
+++ PatientRecord.java
@@ -3,11 +3,14 @@
  * @author kevinmee
  */
 public class PatientRecord {
-    private String patientName;
+    private String patientName, medication;
+    private int dosage;
     
     
     public PatientRecord(){
         patientName = "";
+        medication = "";
+        dosage = 0;
     }
     
     
