086f9b18fe4ff9a61d28ec72c00ce100d659733a
Ramsey Hathout <godsbane@gmail.com>
2012-05-02 23:41:48 -0400

    added singleton comments for simcontroller

--- src/SimulationController.java
+++ src/SimulationController.java
@@ -3,6 +3,27 @@ import java.util.HashMap;
 
 // @author Peter Collins
 
+/*
+ *       //This is the structure of a singleton in java. I believe we should implement this for this class.
+ * public class Singleton {
+ *       // Private constructor prevents instantiation from other classes
+ *       private Singleton() { }
+ * 
+ *       
+ *       //SingletonHolder is loaded on the first execution of Singleton.getInstance() 
+ *       //or the first access to SingletonHolder.INSTANCE, not before.
+ *       
+ *       private static class SingletonHolder { 
+ *               public static final Singleton instance = new Singleton();
+ *       }
+ * 
+ *       public static Singleton getInstance() {
+ *               return SingletonHolder.instance;
+ *       }
+ * }
+ * 
+ */
+
 public class SimulationController {
 
 	private DataIO dataIO = new DataIO();
