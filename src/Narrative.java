f3bd1f068c706743ca71a4992e876c768456c152
kevinmee <kevinmee@192.168.226.28>
2012-05-03 18:37:33 -0400

    Moved the narrative to the src folder because Eric told me to complete it. I will finish implementing everything later tonight, but I am at work so I need to do some work lol

---
+++ src/Narrative.java
@@ -0,0 +1,102 @@
+import java.io.Serializable;
+
+/**
+ * the narratives that student sees for the patient previous record.
+ */
+public class Narrative  implements Serializable{
+    /**
+     */
+    private String date ;
+
+    /**
+     */
+    private String time ;
+
+    /**
+     */
+    private String narrative;
+
+    /**
+     */
+    private String followUP ;
+
+    /**
+     */
+    private String initials;
+
+    public Narrative(String date, String time, String narrative, String followUP, String initials) {
+        this.date = date;
+        this.time = time;
+        this.narrative = narrative;
+        this.followUP = followUP;
+        this.initials = initials;
+    }
+    
+    
+
+    /**
+     * @return 
+     */
+    public String getDate() {
+        return null;
+    }
+
+    /**
+     * @param date 
+     */
+    public void setDate(String date) {
+    }
+
+    /**
+     * @return 
+     */
+    public String getTime() {
+        return null;
+    }
+
+    /**
+     * @param time 
+     */
+    public void setTime(String time) {
+    }
+
+    /**
+     * @return 
+     */
+    public String getNarrative() {
+        return null;
+    }
+
+    /**
+     * @param narrative 
+     */
+    public void setNarrative(String narrative) {
+    }
+
+    /**
+     * @return 
+     */
+    public String getFollowUP() {
+        return null;
+    }
+
+    /**
+     * @param followUP 
+     */
+    public void setFollowUP(String followUP) {
+    }
+
+    /**
+     * @return 
+     */
+    public String getInitials() {
+        return null;
+    }
+
+    /**
+     * @param initials 
+     */
+    public void setInitials(String initials) {
+    }
+}
+
