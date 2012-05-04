91f9fefe0f372e22fac61b71b10c826ebe4e6895
Peter Collins <p_collins@post.massbay.edu>
2012-05-03 23:06:58 -0400

    Merged medication and scenario classes

--- src/CS225FinalProject/DataStructure/Medication.java
+++ src/CS225FinalProject/DataStructure/Medication.java
@@ -1,65 +1,66 @@
 package CS225FinalProject.DataStructure;
 
+/**
+ * @author Keith
+ * @author Eric
+ * 
+ */
+
 import java.io.Serializable;
 import java.lang.*;
 
-/**
- */
-public class Medication implements Serializable{
-    /**
-     */
-    private String medicationName;
+public class Medication implements Serializable {
 
-    /**
-     */
-    private String dosage;
+	private String medicationName;
+	private String dosage;
+	private String medicationDueTimes;
 
-    /**
-     */
-    private String medicationDueTimes;
+	public Medication(String medicationName, String dosage,
+			String medicationDueTimes) {
+		this.medicationName = medicationName;
+		this.dosage = dosage;
+		this.medicationDueTimes = medicationDueTimes;
+	}
 
-    public Medication(String medicationName, String dosage, String medicationDueTimes) {
-        this.medicationName = medicationName;
-        this.dosage = dosage;
-        this.medicationDueTimes = medicationDueTimes;
-    }
-    /**
-     * @return 
-     */
-    public String getMedicationName() {
-        return null;
-    }
+	/**
+	 * @return
+	 */
+	public String getMedicationName() {
+		return medicationDueTimes;
+	}
 
-    /**
-     * @param medicationName 
-     */
-    public void setMedicationName(String medicationName) {
-    }
+	/**
+	 * @param medicationName
+	 */
+	public void setMedicationName(String medicationName) {
+		this.medicationName = medicationName;
+	}
 
-    /**
-     * @return 
-     */
-    public String getDosage() {
-        return null;
-    }
+	/**
+	 * @return
+	 */
+	public String getDosage() {
+		return dosage;
+	}
 
-    /**
-     * @param dosage 
-     */
-    public void setDosage(String dosage) {
-    }
+	/**
+	 * @param dosage
+	 */
+	public void setDosage(String dosage) {
+		this.dosage = dosage;
+	}
 
-    /**
-     * @return 
-     */
-    public int[] getMedicationDueTimes() {
-        return null;
-    }
+	/**
+	 * @return
+	 */
+	public int[] getMedicationDueTimes() {
+		return null;
+	}
 
-    /**
-     * @param medicationDueTimes 
-     */
-    public void setMedicationDueTimes(String medicationDueTimes) {
-    }
+	/**
+	 * @param medicationDueTimes
+	 */
+	public void setMedicationDueTimes(String medicationDueTimes) {
+		this.medicationDueTimes = medicationDueTimes;
+	}
 }
-
--- src/CS225FinalProject/DataStructure/Scenario.java
+++ src/CS225FinalProject/DataStructure/Scenario.java
@@ -1,170 +1,195 @@
 package CS225FinalProject.DataStructure;
 
 import java.io.Serializable;
-import java.lang.*;
 import java.util.ArrayList;
 
 /**
+ * 
+ * @author Kevin Mee
+ * @authoer Eric
  */
-public class Scenario implements Serializable{
-    /**
-     */
-    private ArrayList<Medication> medicationList;
 
-    /**
-     */
-    private String summary;
+public class Scenario implements Serializable {
 
-    /**
-     */
-    private String patientName;
+	private ArrayList<Medication> medicationList;
 
-    /**
-     */
-    private ArrayList<Narrative> narrativeList;
+	private ArrayList<Narrative> narrativeList;
 
-    /**
-     */
-    private int roomNumber;
+	private String summary;
 
-    /**
-     */
-    private String diagnosis;
+	private String patientName;
 
-    /**
-     */
-    private String allergies;
+	private int roomNumber;
 
-    /**
-     */
-    private String scenarioName;
-    /**
-     * counter of how many narratives the scenario has
-     */
-    private int startNumOfNarratives;
+	private String diagnosis;
 
-    public Scenario() {
-    }
+	private String allergies;
 
-    public Scenario(ArrayList<Medication> medicationList, String summary, String patientName, ArrayList<Narrative> narrativeList, int roomNumber, String diagnosis, String allergies, String scenarioName) {
-        this.medicationList = medicationList;
-        this.summary = summary;
-        this.patientName = patientName;
-        this.narrativeList = narrativeList;
-        this.roomNumber = roomNumber;
-        this.diagnosis = diagnosis;
-        this.allergies = allergies;
-        this.scenarioName = scenarioName;
-        startNumOfNarratives=0;
-    }
-    
+	private String scenarioName;
+	/**
+	 * counter of how many narratives the scenario has
+	 */
+	private int startNumOfNarratives;
 
-    /**
-     * @return 
-     */
-    public ArrayList<Medication> getMedicationList() {
-        return null;
-    }
-    
+	private PatientRecord patientRecord;
 
-    /**
-     * @return 
-     */
-    public String getSummary() {
-        return null;
-    }
+	private String description;
 
-    /**
-     * @param summary 
-     */
-    public void setSummary(String summary) {
-    }
-
-    /**
-     * @return 
-     */
-    public String getPatientName() {
-        return null;
-    }
-
-    /**
-     * @param patientName 
-     */
-    public void setPatientName(String patientName) {
-    }
-
-    /**
-     * @return 
-     */
-    public ArrayList<Narrative> getNarrativeList() {
-        return null;
-    }
-
-    /**
-     * @param narrativeList 
-     */
-    public void setNarrativeList(ArrayList<Narrative> narrativeList) {
-        this.narrativeList=narrativeList;
-        startNumOfNarratives=narrativeList.size();
-    }
-    public void addNarrative(Narrative narrative){
-        narrativeList.add(narrative);
-        startNumOfNarratives++;
-    }
-    public void removeNarrative(int i){
-        narrativeList.remove(i);
-        startNumOfNarratives--;
-    }
-    public boolean removeNarrative(Narrative narrative){
-        return narrativeList.remove(narrative);
-    }
-
-    /**
-     * @return 
-     */
-    public int getRoomNumber() {
-        return roomNumber;
-    }
-
-    /**
-     * @param roomNumber 
-     */
-    public void setRoomNumber(int roomNumber) {
-    }
-
-    /**
-     * @return 
-     */
-    public String getDiagnosis() {
-        return null;
-    }
-
-    /**
-     * @param diagnosis 
-     */
-    public void setDiagnosis(String diagnosis) {
-    }
-
-    /**
-     * @return 
-     */
-    public String getAllergies() {
-        return null;
-    }
-
-    /**
-     * @param allergies 
-     */
-    public void setAllergies(String allergies) {
-    }
-    
-    public void addNarrative(String date,String time, String narrative, String followUp, String initials){
-        narrativeList.add(new Narrative(date, time, narrative, followUp, initials));
-    }
-
-	public Object getScenarioName() {
-		// TODO Auto-generated method stub
-		return null;
+	public Scenario() {
 	}
-}
 
+	public Scenario(ArrayList<Medication> medicationList, String summary,
+			String patientName, ArrayList<Narrative> narrativeList,
+			int roomNumber, String diagnosis, String allergies,
+			String scenarioName) {
+
+		this.medicationList = medicationList;
+		this.summary = summary;
+		this.patientName = patientName;
+		this.narrativeList = narrativeList;
+		this.roomNumber = roomNumber;
+		this.diagnosis = diagnosis;
+		this.allergies = allergies;
+		this.scenarioName = scenarioName;
+		startNumOfNarratives = 0;
+	}
+
+	public Scenario(String descript) {
+		description = descript;
+		patientRecord = new PatientRecord();
+		patientName = patientRecord.getName();
+	}
+
+	public ArrayList<Medication> getMedicationList() {
+		return medicationList;
+	}
+
+	public String getSummary() {
+		return summary;
+	}
+
+	public void setSummary(String summary) {
+		this.summary = summary;
+	}
+
+	/**
+	 * @return
+	 */
+	public String getPatientName() {
+		return patientName;
+	}
+
+	/**
+	 * @param patientName
+	 */
+	public void setPatientName(String patientName) {
+		this.patientName = patientName;
+	}
+
+	/**
+	 * @return
+	 */
+	public ArrayList<Narrative> getNarrativeList() {
+		return narrativeList;
+	}
+
+	/**
+	 * @param narrativeList
+	 */
+	public void setNarrativeList(ArrayList<Narrative> narrativeList) {
+		this.narrativeList = narrativeList;
+		startNumOfNarratives = narrativeList.size();
+	}
+
+	public void addNarrative(Narrative narrative) {
+		narrativeList.add(narrative);
+		startNumOfNarratives++;
+	}
+
+	public void removeNarrative(int i) {
+		narrativeList.remove(i);
+		startNumOfNarratives--;
+	}
+
+	public boolean removeNarrative(Narrative narrative) {
+		return narrativeList.remove(narrative);
+	}
+
+	/**
+	 * @return
+	 */
+	public int getRoomNumber() {
+		return roomNumber;
+	}
+
+	/**
+	 * @param roomNumber
+	 */
+	public void setRoomNumber(int roomNumber) {
+		this.roomNumber = roomNumber;
+	}
+
+	public String getDiagnosis() {
+		return patientRecord.getDiagnosis();
+	}
+
+	public void setDiagnosis(String diagnosis) {
+		patientRecord.setDiagnosis(diagnosis);
+	}
+
+	public String getAllergies() {
+		return patientRecord.getAllergies();
+	}
+
+	public void setAllergies(String allergy) {
+
+		patientRecord.setAllergies(allergy);
+	}
+
+	public void addNarrative(String date, String time, String narrative,
+			String followUp, String initials) {
+		narrativeList.add(new Narrative(date, time, narrative, followUp,
+				initials));
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String descript) {
+		description = descript;
+	}
+
+	public String getScenario() {
+		return patientRecord.toString() + "\n" + description;
+	}
+
+	public void setRoom(int room) {
+		patientRecord.setRoom(room);
+	}
+
+	public int getRoom() {
+		return patientRecord.getRoom();
+	}
+
+	public void setAge(int age) {
+		patientRecord.setAge(age);
+	}
+
+	public int getAge() {
+		return patientRecord.getAge();
+	}
+
+	public String recordToString() {
+		return patientRecord.toString();
+	}
+
+	public void setScenarioName(String name) {
+		scenarioName = name;
+	}
+
+	public String getScenarioName() {
+		return scenarioName;
+	}
+
+}
