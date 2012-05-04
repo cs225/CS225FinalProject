b84e34c4d5ac0ca4be229d34c6e1c9c881b902d5
esantana1 <>
2012-05-04 12:08:35 -0400

    Gui Login window fixed

--- src/CS225FinalProject/GUI/LoginGUI.java
+++ src/CS225FinalProject/GUI/LoginGUI.java
@@ -49,127 +49,71 @@ public class LoginGUI extends javax.swing.JDialog {
 	 */
 	@SuppressWarnings("unchecked")
 	// <editor-fold defaultstate="collapsed"
-	// desc="Generated Code">//GEN-BEGIN:initComponents
-	private void initComponents() {
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
 
-		jLabel1 = new javax.swing.JLabel();
-		usernameTextField = new javax.swing.JTextField();
-		jLabel2 = new javax.swing.JLabel();
-		passwordField = new javax.swing.JPasswordField();
-		jButton1 = new javax.swing.JButton();
-		jLabel3 = new javax.swing.JLabel();
-		jLabel4 = new javax.swing.JLabel();
-		jScrollPane1 = new javax.swing.JScrollPane();
-		jList1 = new javax.swing.JList();
-		jButton2 = new javax.swing.JButton();
-		professorLoginButton = new javax.swing.JButton();
+        jLabel1 = new javax.swing.JLabel();
+        usernameTextField = new javax.swing.JTextField();
+        jLabel2 = new javax.swing.JLabel();
+        passwordField = new javax.swing.JPasswordField();
+        jButton1 = new javax.swing.JButton();
+        jLabel3 = new javax.swing.JLabel();
+        jButton2 = new javax.swing.JButton();
+        professorLoginButton = new javax.swing.JButton();
 
-		setDefaultCloseOperation(javax.swing.WindowConstants.DO_NOTHING_ON_CLOSE);
-		setAlwaysOnTop(true);
-		getContentPane().setLayout(
-				new org.netbeans.lib.awtextra.AbsoluteLayout());
+        setDefaultCloseOperation(javax.swing.WindowConstants.DO_NOTHING_ON_CLOSE);
+        setAlwaysOnTop(true);
+        getContentPane().setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
 
-		jLabel1.setText("UserName:");
-		getContentPane().add(
-				jLabel1,
-				new org.netbeans.lib.awtextra.AbsoluteConstraints(53, 103, 65,
-						27));
+        jLabel1.setText("UserName:");
+        getContentPane().add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(53, 103, 65, 27));
 
-		usernameTextField
-				.addActionListener(new java.awt.event.ActionListener() {
-					public void actionPerformed(java.awt.event.ActionEvent evt) {
-						usernameTextFieldActionPerformed(evt);
-					}
-				});
-		getContentPane().add(
-				usernameTextField,
-				new org.netbeans.lib.awtextra.AbsoluteConstraints(120, 110,
-						150, -1));
+        usernameTextField.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                usernameTextFieldActionPerformed(evt);
+            }
+        });
+        getContentPane().add(usernameTextField, new org.netbeans.lib.awtextra.AbsoluteConstraints(120, 110, 150, -1));
 
-		jLabel2.setText("Password:");
-		getContentPane().add(
-				jLabel2,
-				new org.netbeans.lib.awtextra.AbsoluteConstraints(53, 139, -1,
-						-1));
+        jLabel2.setText("Password:");
+        getContentPane().add(jLabel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(53, 139, -1, -1));
 
-		passwordField.setText("test");
-		passwordField.addKeyListener(new java.awt.event.KeyAdapter() {
-			public void keyPressed(java.awt.event.KeyEvent evt) {
-				passwordFieldKeyPressed(evt);
-			}
-		});
-		getContentPane().add(
-				passwordField,
-				new org.netbeans.lib.awtextra.AbsoluteConstraints(120, 140,
-						150, -1));
+        passwordField.addKeyListener(new java.awt.event.KeyAdapter() {
+            public void keyPressed(java.awt.event.KeyEvent evt) {
+                passwordFieldKeyPressed(evt);
+            }
+        });
+        getContentPane().add(passwordField, new org.netbeans.lib.awtextra.AbsoluteConstraints(120, 140, 150, -1));
 
-		jButton1.setText("Student Login");
-		jButton1.addActionListener(new java.awt.event.ActionListener() {
-			public void actionPerformed(java.awt.event.ActionEvent evt) {
-				jButton1ActionPerformed(evt);
-			}
-		});
-		getContentPane().add(
-				jButton1,
-				new org.netbeans.lib.awtextra.AbsoluteConstraints(160, 270, -1,
-						-1));
+        jButton1.setText("Student Login");
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton1ActionPerformed(evt);
+            }
+        });
+        getContentPane().add(jButton1, new org.netbeans.lib.awtextra.AbsoluteConstraints(140, 180, -1, -1));
 
-		jLabel3.setIcon(new javax.swing.ImageIcon(getClass().getResource(
-				"/res/massbayLogo.png"))); // NOI18N
-		getContentPane().add(
-				jLabel3,
-				new org.netbeans.lib.awtextra.AbsoluteConstraints(80, 11, -1,
-						-1));
+        jLabel3.setIcon(new javax.swing.ImageIcon(getClass().getResource("/res/massachusetts-bay-community-college.jpg"))); // NOI18N
+        getContentPane().add(jLabel3, new org.netbeans.lib.awtextra.AbsoluteConstraints(80, 11, -1, -1));
 
-		jLabel4.setText("Session:");
-		getContentPane().add(
-				jLabel4,
-				new org.netbeans.lib.awtextra.AbsoluteConstraints(60, 170, -1,
-						-1));
+        jButton2.setText("Exit");
+        jButton2.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton2ActionPerformed(evt);
+            }
+        });
+        getContentPane().add(jButton2, new org.netbeans.lib.awtextra.AbsoluteConstraints(260, 180, -1, -1));
 
-		jList1.setModel(new javax.swing.AbstractListModel() {
-			String[] strings = { "NR101", "NR102", "NR103", "NR150", "NR200" };
+        professorLoginButton.setText("login Proffesor");
+        professorLoginButton.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                professorLoginButtonActionPerformed(evt);
+            }
+        });
+        getContentPane().add(professorLoginButton, new org.netbeans.lib.awtextra.AbsoluteConstraints(20, 180, -1, -1));
 
-			public int getSize() {
-				return strings.length;
-			}
-
-			public Object getElementAt(int i) {
-				return strings[i];
-			}
-		});
-		jScrollPane1.setViewportView(jList1);
-
-		getContentPane().add(
-				jScrollPane1,
-				new org.netbeans.lib.awtextra.AbsoluteConstraints(122, 174,
-						150, 90));
-
-		jButton2.setText("Exit");
-		jButton2.addActionListener(new java.awt.event.ActionListener() {
-			public void actionPerformed(java.awt.event.ActionEvent evt) {
-				jButton2ActionPerformed(evt);
-			}
-		});
-		getContentPane().add(
-				jButton2,
-				new org.netbeans.lib.awtextra.AbsoluteConstraints(270, 270, -1,
-						-1));
-
-		professorLoginButton.setText("login Proffesor");
-		professorLoginButton
-				.addActionListener(new java.awt.event.ActionListener() {
-					public void actionPerformed(java.awt.event.ActionEvent evt) {
-						professorLoginButtonActionPerformed(evt);
-					}
-				});
-		getContentPane().add(
-				professorLoginButton,
-				new org.netbeans.lib.awtextra.AbsoluteConstraints(30, 270, -1,
-						-1));
-
-		pack();
-	}// </editor-fold>//GEN-END:initComponents
+        pack();
+    }// </editor-fold>//GEN-END:initComponents
 
 	private void usernameTextFieldActionPerformed(java.awt.event.ActionEvent evt) {// GEN-FIRST:event_usernameTextFieldActionPerformed
 		// TODO add your handling code here:
@@ -284,17 +228,14 @@ public class LoginGUI extends javax.swing.JDialog {
 		});
 	}
 
-	// Variables declaration - do not modify//GEN-BEGIN:variables
-	private javax.swing.JButton jButton1;
-	private javax.swing.JButton jButton2;
-	private javax.swing.JLabel jLabel1;
-	private javax.swing.JLabel jLabel2;
-	private javax.swing.JLabel jLabel3;
-	private javax.swing.JLabel jLabel4;
-	private javax.swing.JList jList1;
-	private javax.swing.JScrollPane jScrollPane1;
-	private javax.swing.JPasswordField passwordField;
-	private javax.swing.JButton professorLoginButton;
-	private javax.swing.JTextField usernameTextField;
-	// End of variables declaration//GEN-END:variables
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton jButton1;
+    private javax.swing.JButton jButton2;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel3;
+    private javax.swing.JPasswordField passwordField;
+    private javax.swing.JButton professorLoginButton;
+    private javax.swing.JTextField usernameTextField;
+    // End of variables declaration//GEN-END:variables
 }
