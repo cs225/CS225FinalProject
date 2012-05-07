20cf7d9ac364dd669dfd5b550431ba830e987d0b
kevinmee <kevinmee@192.168.226.28>
2012-05-06 23:58:04 -0400

    Fixed the returns

--- src/CS225FinalProject/Validators/Evaluator.java
+++ src/CS225FinalProject/Validators/Evaluator.java
@@ -12,82 +12,103 @@ import javax.swing.JTable;
 /**
  */
 public class Evaluator {
+
     private Scanner fileScan;
-    private String unit, intUnit, daily, everyOther, trailing0, morphine, 
+    private String unit, intUnit, daily, everyOther, trailing0, morphine,
             sulfate, magSulfate, microgram, halfStrength, threeWeek,
-            subcutaneous1, subcutaneous2, discharge, cubic, rightEar, leftEar, 
+            subcutaneous1, subcutaneous2, discharge, cubic, rightEar, leftEar,
             bothEars, leftEye, rightEye, bothEyes, result;
 
-    public String giveSuggestion(String input){
-    fileScan = new Scanner(input);
-    
-    unit = "u";
-    intUnit = "iu";
-    daily = "Q.D.";
-    everyOther = "Q.O.D.";
-    trailing0 = ".0";
-    morphine = "MS";
-    sulfate = "MSO4";
-    magSulfate = "MgSO4";
-    microgram = "ug";
-    halfStrength = "H.S.";
-    threeWeek = "T.I.W.";
-    subcutaneous1 = "S.C.";
-    subcutaneous2 = "S.Q.";
-    discharge = "D/C";
-    cubic = "c.c.";
-    rightEar = "A.S.";
-    leftEar = "A.D.";
-    bothEars = "A.U.";
-    rightEye = "O.S.";
-    leftEye = "O.D.";
-    bothEyes = "O.U.";
-    
-    if(fileScan.hasNext(unit))
-        result += unit;
-    else if(fileScan.hasNext(intUnit))
-        result += intUnit;
-    else if(fileScan.hasNext(daily))
-        result += daily;
-    else if(fileScan.hasNext(everyOther))
-        result += everyOther;
-    else if(fileScan.hasNext(trailing0))
-        result += trailing0;
-    else if(fileScan.hasNext(morphine))
-        result += morphine;
-    else if(fileScan.hasNext(sulfate))
-        result += sulfate;
-    else if(fileScan.hasNext(magSulfate))
-        result += magSulfate;
-    else if(fileScan.hasNext(microgram))
-        result += microgram;
-    else if(fileScan.hasNext(halfStrength))
-        result += halfStrength;
-    else if(fileScan.hasNext(threeWeek))
-        result += threeWeek;
-    else if(fileScan.hasNext(subcutaneous1))
-        result += subcutaneous1;
-    else if(fileScan.hasNext(subcutaneous2))
-        result += subcutaneous2;
-    else if(fileScan.hasNext(discharge))
-        result += discharge;
-    else if(fileScan.hasNext(cubic))
-        result += cubic;
-    else if(fileScan.hasNext(rightEar))
-        result += rightEar;
-    else if(fileScan.hasNext(leftEar))
-        result += leftEar;
-    else if(fileScan.hasNext(bothEars))
-        result += bothEars;
-    else if(fileScan.hasNext(rightEye))
-        result += rightEye;
-    else if(fileScan.hasNext(leftEye))
-        result += leftEye;
-    else if(fileScan.hasNext(bothEyes))
-        result += bothEyes;
-    
-    return result;
-    
+    public String giveSuggestion(String input) {
+        fileScan = new Scanner(input);
+
+        unit = "u";
+        intUnit = "iu";
+        daily = "Q.D.";
+        everyOther = "Q.O.D.";
+        trailing0 = ".0";
+        morphine = "MS";
+        sulfate = "MSO4";
+        magSulfate = "MgSO4";
+        microgram = "ug";
+        halfStrength = "H.S.";
+        threeWeek = "T.I.W.";
+        subcutaneous1 = "S.C.";
+        subcutaneous2 = "S.Q.";
+        discharge = "D/C";
+        cubic = "c.c.";
+        rightEar = "A.S.";
+        leftEar = "A.D.";
+        bothEars = "A.U.";
+        rightEye = "O.S.";
+        leftEye = "O.D.";
+        bothEyes = "O.U.";
+
+        if (fileScan.hasNext(unit)) {
+            result += unit + "\n";
+        }
+        if (fileScan.hasNext(intUnit)) {
+            result += intUnit + "\n";
+        }
+        if (fileScan.hasNext(daily)) {
+            result += daily + "\n";
+        }
+        if (fileScan.hasNext(everyOther)) {
+            result += everyOther + "\n";
+        }
+        if (fileScan.hasNext(trailing0)) {
+            result += trailing0 + "\n";
+        }
+        if (fileScan.hasNext(morphine)) {
+            result += morphine + "\n";
+        }
+        if (fileScan.hasNext(sulfate)) {
+            result += sulfate + "\n";
+        }
+        if (fileScan.hasNext(magSulfate)) {
+            result += magSulfate + "\n";
+        }
+        if (fileScan.hasNext(microgram)) {
+            result += microgram + "\n";
+        }
+        if (fileScan.hasNext(halfStrength)) {
+            result += halfStrength + "\n";
+        }
+        if (fileScan.hasNext(threeWeek)) {
+            result += threeWeek + "\n";
+        }
+        if (fileScan.hasNext(subcutaneous1)) {
+            result += subcutaneous1 + "\n";
+        }
+        if (fileScan.hasNext(subcutaneous2)) {
+            result += subcutaneous2 + "\n";
+        }
+        if (fileScan.hasNext(discharge)) {
+            result += discharge + "\n";
+        }
+        if (fileScan.hasNext(cubic)) {
+            result += cubic + "\n";
+        }
+        if (fileScan.hasNext(rightEar)) {
+            result += rightEar + "\n";
+        }
+        if (fileScan.hasNext(leftEar)) {
+            result += leftEar + "\n";
+        }
+        if (fileScan.hasNext(bothEars)) {
+            result += bothEars + "\n";
+        }
+        if (fileScan.hasNext(rightEye)) {
+            result += rightEye + "\n";
+        }
+        if (fileScan.hasNext(leftEye)) {
+            result += leftEye + "\n";
+        }
+        if (fileScan.hasNext(bothEyes)) {
+            result += bothEyes + "\n";
+        }
+
+        return result;
+
     }
 }
-
