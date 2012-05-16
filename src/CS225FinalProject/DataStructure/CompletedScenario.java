47eec67e14745dba584cd01e69313a7331e87cf5
esantana1 <>
2012-05-16 18:19:33 -0400

    Completeted Scenario Implements Printable, Evan look at the instructions on the method. let me know if you have questions

--- src/CS225FinalProject/DataStructure/CompletedScenario.java
+++ src/CS225FinalProject/DataStructure/CompletedScenario.java
@@ -1,5 +1,10 @@
 package CS225FinalProject.DataStructure;
 
+import CS225FinalProject.SimulationManager;
+import java.awt.Graphics;
+import java.awt.print.PageFormat;
+import java.awt.print.Printable;
+import java.awt.print.PrinterException;
 import java.io.Serializable;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
@@ -10,7 +15,7 @@ import java.util.Date;
 import javax.swing.JTable;
 
 //This will hold user session data
-public class CompletedScenario implements Serializable{
+public class CompletedScenario implements Serializable, Printable{
 
 	private Date dateTaken;
 	private Integer score;
@@ -18,6 +23,11 @@ public class CompletedScenario implements Serializable{
 	private Scenario scenarioTaken;
 	private DateFormat dateFormat = new SimpleDateFormat("MM/dd/yyyy HH:mm:ss");
         private  ArrayList<Narrative> studentinput;
+        
+        /**
+         * The student who took the test, only useful for the printing implementation.
+         */
+        private Student student;
 
 	// finish constructor, getters and setters have been generated.
 	// storing data propely is not implemented.
@@ -35,6 +45,8 @@ public class CompletedScenario implements Serializable{
 		this.scenarioTaken = scenarioTaken;
                 this.studentinput = studentinput;
                 score=null;
+                student = (Student)SimulationManager.CURRENT_USER;
+                
 	}
 	public String getDateTaken() {
 		// returns a formatted string representing the exact date and time the
@@ -73,4 +85,16 @@ public class CompletedScenario implements Serializable{
 	public void setScenarioTaken(Scenario scenarioTaken) {
 		this.scenarioTaken = scenarioTaken;
 	}
+
+        
+        /**
+         * Evan, implement the report here. it should print the details like the suggestion and score and student info.
+         * I made a instance of the Student that took the simulation so you can display their info.
+         * @throws PrinterException 
+         */
+    @Override
+    public int print(Graphics graphics, PageFormat pageFormat, int pageIndex) throws PrinterException {
+        throw new UnsupportedOperationException("Not supported yet.");
+        //student.getRealName(); sample of getting the name of the student . it is functional :).
+    }
 }
